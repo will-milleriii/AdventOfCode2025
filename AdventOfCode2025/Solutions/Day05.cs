ce7d468e45521cb0abe49cce59187ee5bffd5c9a
will-milleriii <willmiller629@gmail.com>
2025-12-05 14:27:31 -0500

    day 5 p1

---
+++ AdventOfCode2025/Solutions/Day05.cs
@@ -0,0 +1,66 @@
+﻿using AdventOfCode2025.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2025.Solutions
+{
+    public class Day05
+    {
+
+        public long Part1()
+        {
+            var filePath = @"C:\Users\willm\source\repos\AdventOfCode2025\AdventOfCode2025\Inputs\Day05.txt";
+
+            var input = FileReader.ReadLines(filePath);
+
+            var index = input.IndexOf("");
+           
+            var ingredientIds = input[..index];
+            var ingredients = input[(index+1)..];
+            ingredients.Sort();
+            ingredientIds.Sort();
+
+            var validIngredientRecords = new List<Range>(input.Length);
+            var validFoundIngredients = new List<long>();
+
+            foreach (var ingredientSpan in ingredientIds)
+            {
+                var splitIndex = ingredientSpan.IndexOf("-");
+                var minInt = long.Parse(ingredientSpan[..splitIndex]);
+                var maxInt = long.Parse(ingredientSpan[(splitIndex+1)..]);
+
+                validIngredientRecords.Add(new Range(minInt, maxInt));
+
+            }
+
+            foreach (var possibleIngredient in ingredients)
+            {
+                if (IsValidIngredient(validIngredientRecords, long.Parse(possibleIngredient)))
+                {
+                    validFoundIngredients.Add(long.Parse(possibleIngredient));
+                }
+
+            }
+            var totalValidIngredients = validFoundIngredients.Count;
+            return totalValidIngredients;
+        }
+
+        public long Part2()
+        {
+            return 2;
+        }
+
+        private bool IsValidIngredient(List<Range> ranges, long value)
+        {
+
+            foreach (var range in ranges)
+            {
+                if (value >= range.Min && value <= range.Max)
+                    return true;
+            }
+            return false;
+        }
+        record Range(long Min, long Max);
+    }
+}
--- AdventOfCode2025/obj/Debug/net10.0/AdventOfCode2025.AssemblyInfo.cs
+++ AdventOfCode2025/obj/Debug/net10.0/AdventOfCode2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8611650d7cd909bbee2d98249f745a6ba44d5d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8643eb12ad9687dc9ec550285a6643d62c37b3ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
