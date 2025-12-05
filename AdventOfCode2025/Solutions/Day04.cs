8643eb12ad9687dc9ec550285a6643d62c37b3ee
will-milleriii <willmiller629@gmail.com>
2025-12-05 12:57:54 -0500

    day 4 p2

--- AdventOfCode2025/Solutions/Day04.cs
+++ AdventOfCode2025/Solutions/Day04.cs
@@ -54,14 +54,70 @@ namespace AdventOfCode2025.Solutions
             return total;
         }
 
+        public long Part2()
+        {
+            var filePath = @"C:\Users\willm\source\repos\AdventOfCode2025\AdventOfCode2025\Inputs\Day04.txt";
+
+            var input = FileReader.ReadLines(filePath);
+
+            int[] dirRow = { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] dirCol = { -1, 0, 1, -1, 1, -1, 0, 1 };
+            const int Directions = 8;
+
+            const char Value = '@';
+
+            var total = 0;
+
+            var rows = input.Length;
+            var cols = input[0].Length;
+
+            var changed = true;
+
+            while (changed)
+            {
+                changed = false;
+                // loop through row first
+                for (var i = 0; i < rows; i++)
+                {
+                    //now loop through columns. this kind of sucks
+                    for (var j = 0; j < cols; j++)
+                    {
+                        // gtfo if we don't have the char
+                        if (input[i][j] != Value)
+                            continue;
+
+                        // now loop through indices
+                        var matches = 0;
+                        for (var k = 0; k < Directions; k++)
+                        {
+                            var newRowIndex = i + dirRow[k];
+                            var newColIndex = j + dirCol[k];
+
+                            if (IsIndexInbounds(newRowIndex, newColIndex, input) && input[newRowIndex][newColIndex] is Value)
+                                matches++;
+                        }
+
+                        if (matches < 4)
+                        {
+                            //start transformation logic for valid roll
+                            var rowAsCharArr = input[i].ToCharArray();
+                            rowAsCharArr[j] = 'X'; // TODO: make a const
+                            input[i] = new string(rowAsCharArr);
+                            changed = true;
+                            total++;
+                        }
+
+                    }
+                }
+            }
+            return total;
+        }
+
         public bool IsIndexInbounds(int row, int col, string[] input)
         {
             return row >= 0 && row < input.Length && col >= 0 && col < input[0].Length;
         }
 
-        public long Part2()
-        {
-            return 2;
-        }
+       
     }
 }
--- AdventOfCode2025/obj/Debug/net10.0/AdventOfCode2025.AssemblyInfo.cs
+++ AdventOfCode2025/obj/Debug/net10.0/AdventOfCode2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5abb57bbfc90c0192556b92e9f02c07ecac3058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8611650d7cd909bbee2d98249f745a6ba44d5d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
